0d68ce4cd419acfcf67eecbf852ea750d32e8389
Valentík Lukáš <Valentik.Lu.2022@skola.ssps.cz>
2022-11-08 11:26:22 +0100

    v1 final

---
+++ test-uloha/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace test_uloha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool start = true;
+            while (start == true)
+            {
+                Console.WriteLine();
+                Console.Write("Zadejte číslo: ");
+                string x = Console.ReadLine();
+                if (x == "n")
+                {
+                    Console.WriteLine("Ukončuji program...");
+                    start = false;
+                }
+                else
+                {
+                    int n = int.Parse(x);
+                    int z = n % 2;
+                    if (z > 0)
+                    {
+                        Console.WriteLine("Vaše číslo je liché");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Vaše číslo je sudé");
+                    }
+                }
+
+
+
+            }
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
